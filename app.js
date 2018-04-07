64cf14979216f2f6715a3b0dc9ecf1e702f8bef0
amanovishnu <amanovishnu@gmail.com>
2018-04-08 05:15:05 +0530

    GenreAdded

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+Genre = require('./models/genre');
+//Connection to Database
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
 
-app.get('/',(req,res)=>{
-    res.write('Hello World');
+app.get('/',function(req,res){
+    res.send('Please Use /api/books or /api/genre');
 });
 
-app.listen(3000,()=>{
-    console.log('Server Running on Port 3000');
+app.get('/api/books',function(req,res){
+    res.send('Hello to books');
+});
+
+app.get('/api/genres',function(req,res){
+    Genre.getGenres(function(err,genres){
+        if(err){
+            throw err;
+        }
+        res.json(genres);
+    });
+});
+
+app.listen(3000,function(){
+    console.log('Server Running on Port  3000');
 });
